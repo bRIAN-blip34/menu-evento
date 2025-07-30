4a90fb738f60844f6fdb7a6622e514396850fb90
Brian Oficina <oficinabrian3@gmail.com>
2025-07-30 16:56:37 -0300

    Primer subida

---
+++ precios.js
@@ -0,0 +1,13 @@
+
+const precios = {
+  "Vodka": 1000,
+  "Whisky": 1000,
+  "Tequila": 1000,
+  "Coñac": 1000,
+  "Aguardiente": 1000,
+  "Cerveza artesanal": 1000,
+  "Cerveza": 1000,
+  "Agua sin gas": 1000,
+  "Agua con Gas": 1000,
+  "Vino": 1000,
+};
