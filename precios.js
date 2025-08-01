bfe1168e3ae7bd3060e589eca1fadd3f81780ba8
Brian Oficina <oficinabrian3@gmail.com>
2025-08-01 19:24:33 -0300

    Actualización

--- precios.js
+++ precios.js
@@ -1,13 +1,12 @@
-
 const precios = {
-  "Vodka": 1000,
-  "Whisky": 1000,
-  "Tequila": 1000,
-  "Coñac": 1000,
-  "Aguardiente": 1000,
-  "Cerveza artesanal": 1000,
-  "Cerveza": 1000,
-  "Agua sin gas": 1000,
-  "Agua con Gas": 1000,
-  "Vino": 1000,
+  "Vodka": { precio: 3000, icono: "vodka.png" },
+  "Whisky": { precio: 10000, icono: "whisky.png" },
+  "Champagne": { precio: 14000, icono: "champan.png" },
+  "Coca-Cola": { precio: 1000, icono: "Coca-Cola.png" },
+  "Fernet": { precio: 9000, icono: "fernet.png" },
+  "Jugo de Naranja": { precio: 900, icono: "zumo-de-naranja.png" },
+  "Cerveza": { precio: 1000, icono: "cerveza.png" },
+  "Agua sin gas": { precio: 500, icono: "botella-de-agua.png" },
+  "Agua con gas": { precio: 1000, icono: "agua-con-gas.png" },
+  "Vino": { precio: 5000, icono: "vino.png" }
 };
